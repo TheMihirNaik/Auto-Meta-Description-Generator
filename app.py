82ba6cf4366993327b3247dfe5d51eacc6530122
mihir23192 <mihir23192@gmail.com>
2021-09-06 09:20:05 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import urllib
+import requests
+from bs4 import BeautifulSoup
+import re
+import requests
+import html2text
+import re
+from transformers import pipeline
+import os
+from transformers import PegasusTokenizer, PegasusForConditionalGeneration
+
+# Set headers
+headers = requests.utils.default_headers()
+headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
+
+st.title("🔎 Auto Meta Description Generator")
+
+url = st.text_input("")
+if url:
+  req = requests.get(url, headers)
+  #st.markdown("Status : URL Scraped")
+  
+  soup = BeautifulSoup(req.content, 'html.parser')
+  html = soup.prettify()
+  #st.markdown("Status : HTML Parsed")
+  
+  h = html2text.HTML2Text()
+  html_to_text = h.handle(html)
+  
+  st.text("Status : URL scraped, HTMP Parsed & Cleaned. The content is ready for Summarization.")
+
+  st.caption("Existing Meta Description")
+  find_description = soup.find('meta', {'name':'description'})
+  existing_description = find_description['content']
+  st.write(existing_description)
+  
+  os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+  
+  st.text("Status : Pytorch Summarizer Initiated. It will take around 2 minutes to get up to speed")
+
+  summarizer = pipeline("summarization", model="t5-base", tokenizer="t5-base", framework="tf")
+  st.text("Status : Pytorch Summarizer is ready to summarize. New Description Generated is being generated. Give a few more seconds.")
+  
+  
+  new_description = summarizer(html_to_text, max_length=160)
+
+  st.caption("New Auto Generated Meta Description - T5")
+  st.write(new_description)
