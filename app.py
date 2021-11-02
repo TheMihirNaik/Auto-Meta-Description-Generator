39eac94955babd4312ea1ef931b2b96c307c6b44
Mihir Naik <mihir23192@gmail.com>
2021-11-02 13:36:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,99 +1,86 @@
+#import python packages
 import streamlit as st
 from transformers import pipeline
-from bs4 import BeautifulSoup
+
 import requests
-import urllib
+from bs4 import BeautifulSoup
+import html2text
+import requests
 import re
 import mistletoe
-import html2text
 from mistletoe import markdown
 from html2text import HTML2Text
 
-# Set headers
-headers = requests.utils.default_headers()
-headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
-
-st.text("Transformer just initiated. It will take around 60 seconds for URL input box to show.")
-summarizer = pipeline("summarization")
+import torch
+import json 
+from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
+from transformers import pipeline
 
 
-st.title("🔎 Auto Meta Description Generator")
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
 
-url = st.text_input("")
+import nltk
+nltk.download('punkt')
+
+
+st.title("🔎 :pencil: Auto Meta Description Generator")
+
+st.info("Status : Transformers t5-base Summarization Pipeline is being initialized.")
+t5_summarizer = pipeline("summarization", model="t5-base", tokenizer="t5-base", framework="tf")
+st.success("Status : Successfully Initialized. You will be able to use this for multiple URLs during session.")
+
+
+url = st.text_input("Enter the URL you want Auto Generated Meta Description for.")
+
+
 if url:
+  # Set headers
+  headers = requests.utils.default_headers()
+  headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
+  
+  st.markdown('URL requested')
   req = requests.get(url, headers)
+
+  st.markdown('HTML Parsed')
   soup = BeautifulSoup(req.content, 'html.parser')
   html = soup.prettify()
 
-  def html2md(html):
-      parser = HTML2Text()
-      parser.ignore_images = True
-      parser.ignore_anchors = True
-      parser.body_width = 0
-      md = parser.handle(html)
-      return md
-
-  def html2plain(html):
-      # HTML to Markdown
-      md = html2md(html)
-      # Normalise custom lists
-      md = re.sub(r'(^|\n) ? ? ?\\?[•·–-—-*]( \w)', r'\1  *\2', md)
-      # Convert back into HTML
-      html_simple = mistletoe.markdown(md)
-      # Convert to plain text
-      soup = BeautifulSoup(html_simple)
-      text = soup.getText()
-      # Strip off table formatting
-      text = re.sub(r'(^|\n)\|\s*', r'\1', text)
-      # Strip off extra emphasis
-      text = re.sub(r'\*\*', '', text)
-      # Remove trailing whitespace and leading newlines
-      text = re.sub(r' *$', '', text)
-      text = re.sub(r'\n\n+', r'\n\n', text)
-      text = re.sub(r'^\n+', '', text)
-      return text
-
-  ARTICLE = html2plain(html)
-  st.text("Status : URL scraped, HTMP Parsed & Cleaned. The content is ready for Summarization.")
+  full_page_text = html2text.html2text(html)
+  st.markdown('HTML converted to Text')
+  #st.markdown(full_page_text)
   
-  max_chunk = 500
+  LANGUAGE = 'english'
+  parser = PlaintextParser.from_string(full_page_text, Tokenizer(LANGUAGE))
 
-  ARTICLE = ARTICLE.replace('.', '.<eos>')
-  ARTICLE = ARTICLE.replace('?', '?<eos>')
-  ARTICLE = ARTICLE.replace('!', '!<eos>')
+  from sumy.summarizers.lsa import LsaSummarizer
 
-  sentences = ARTICLE.split('<eos>')
-  current_chunk = 0 
-  chunks = []
+  #Summarizer Started
+  LSA_summarizer = LsaSummarizer()
 
-  for sentence in sentences:
-      if len(chunks) == current_chunk + 1: 
-          if len(chunks[current_chunk]) + len(sentence.split(' ')) <= max_chunk:
-              chunks[current_chunk].extend(sentence.split(' '))
-          else:
-              current_chunk += 1
-              chunks.append(sentence.split(' '))
-      else:
-          print(current_chunk)
-          chunks.append(sentence.split(' '))
+  #removing stopwords
+  LSA_summarizer.stop_words = get_stop_words(LANGUAGE)
 
-  for chunk_id in range(len(chunks)):
-      chunks[chunk_id] = ' '.join(chunks[chunk_id])
-
-  st.text("Status : Text chunking in less than 500 tokens complete.")
-  st.text("Status : Summarising each chunk.")
-  res = summarizer(chunks, max_length=160, min_length=30, do_sample=False)
+  #Summarize the document with 15 sentences
+  LSA_Summary =LSA_summarizer(parser.document,15)
   
-  res[0]
-  ' '.join([summ['summary_text'] for summ in res])
-  text = ' '.join([summ['summary_text'] for summ in res])
-  st.text("Status : Summaized text from chunks are joined. Now Summarizing for Meta Descrition.")
-  final_text = summarizer(text, max_length=160, min_length=30, do_sample=False)
+
+  LSA_Summary_Text = ""
+
+  for each in LSA_Summary:
+    LSA_Summary_Text = LSA_Summary_Text + str(each)
+  st.info('Generating - LSA based Extractive Summary to use for Abstractive Summurization.')
+  st.caption(LSA_Summary_Text)
   
-  st.markdown("New Auto Generated Meta Description - T5")
-  st.write(final_text)
   
-  st.markdown("Existing Meta Description")
-  find_description = soup.find('meta', {'name':'description'})
-  existing_description = find_description['content']
-  st.write(existing_description)
+
+  st.header(':pencil2: Generating - Meta Description using Extractive Summary.  It takes 2-3 mins.')
+  new_description = t5_summarizer(LSA_Summary_Text, max_length=160, min_length=30, do_sample=False)
+
+  st.info(':pencil: Here is your New Description')
+  st.success(new_description[0]['summary_text'].capitalize())
+  
